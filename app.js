e7f8f0cbcec2725741472f464909250f87678d2f
HongBeom Choi <HongBeom Choi>
2016-09-10 00:18:40 +0900

    merged. start session added.

--- app.js
+++ app.js
@@ -1,48 +1,3 @@
-/*
-'use strict';
-
-const http = require('http');
-const Bot = require('messenger-bot')
-const process = require('process') 
- let bot = new Bot({ 
-     token: 'EAAZAafXETcwABAFz2qNcr1OyWRQukdS0LlNO9MUjbRsOvDgWfp7QcZAg4XaZBVoHJrl2FU5ZAMGo44UmBVRqV8ZCClUwTgXn0Jf4om2JQ8mbBZATzTTIFPOoENG1DW5cYK94I8CtKYnm9yRHSj7BLwQkjnP1Y0ui8W2rdBXvBxiQZDZD',
-     verify: 'hhbot',
-     app_secret: '15fefb7616c05e773ceed7badd94dd15'
-    });
-bot.on('error', (err) => { 
-    console.log('error event : ' + err.message) 
-});
-
-bot.on('message', (payload, reply) => {
-    let  text = "default"
-    if (payload.message.text)
-         text = payload.message.text 
-    console.log(payload.message);
-    if (payload.message.attachments){
-        text += payload.message.attachments[0].payload.coordinates.lat
-        console.log(payload.message.attachments[0].payload.coordinates)
-    }
-    bot.getProfile(payload.sender.id, (err, profile) => {
-
-        if (err){
-            console.log(err.message)
-            //throw err
-        } 
-        reply({text}, (err) => { 
-            if (err) {
-                console.log(err.message) 
-                //throw err
-            }
-             console.log(`${profile.first_name} ${profile.last_name}: ${text}`) 
-            }) 
-        }) 
-    })
-
-
-http.createServer(bot.middleware()).listen(process.env.PORT, () => {
-    console.log(`server listen on ${process.env.PORT}`);
-})
-*/
 'use strict';
 const http = require('http');
 const express = require('express');
@@ -106,7 +61,6 @@ bot.on('postback', (payload, reply) => {
 })
 
 
-let app = express()
 let app = express();
 
 app.use(bodyParser.json());
