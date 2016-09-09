da753fbeb72628d26c80d783f94f336a5bf08714
HongBeom Choi <HongBeom Choi>
2016-09-10 02:54:57 +0900

    refactoring

--- res/Strings.js
+++ res/Strings.js
@@ -2,7 +2,7 @@
  * Created by HongBeom Choi on 9/10/2016.
  */
 module.exports = {
-    KR_GREETING_MSG : '안녕하세요! HHbot 이에요. 저는 여러분의 위치를 기반으로 버스의 정보를 제공하려 해요.  본 서비스는 여러분의 위치정보를 필요로해요. 아! PC로는 어려울 것 같아요.,
+    KR_GREETING_MSG : '안녕하세요! HHbot 이에요. 저는 여러분의 위치를 기반으로 버스의 정보를 제공하려 해요.  본 서비스는 여러분의 위치정보를 필요로해요. 아! PC로는 어려울 것 같아요.',
     KR_NEED_LOCATION : '위치를 전송해 주시겠어요?',
     KR_NEED_BUSNUM : '버스는 ~ 가있네요. 어떤 버스를타실 거에요?',
      KR_VALIDATE_LOCATION : '위치는 ~에요.',
