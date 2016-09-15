fa30e6d1b3441abb345dd39a4749727d4fa11bac
dogfooter <gkstnrkf@ajou.ac.kr>
2016-09-15 18:09:25 +0900

    query_analysis

---
+++ query_analysis/getStation.js
@@ -0,0 +1,34 @@
+const request = require('sync-request');
+
+let headers = {
+    'Origin': 'http://www.gbis.go.kr',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.92 Safari/537.36',
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Referer': 'http://www.gbis.go.kr/gbis2014/schBus.action?mapTabCd=1',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Connection': 'keep-alive',
+    'Cookie': 'PHAROSVISITOR=00004fee01570f1da75f36e9c0a867c9; ACEFCID=UID-57D2B892EA4906C8526E5EF1; weatherChange=2; JSESSIONID=Qx1gZaZzZhfrzcH9YeXYfqHtL16naw5JgGgQUY5LVK3a7FFoQtcIshjSIKQsuTGx.Gbus-WAS_servlet_engine3; _ga=GA1.3.524032906.1473427599; _gat=1; myword=%EC%95%84%EC%A3%BC%EB%8C%80.%EC%95%84%EC%A3%BC%EB%8C%80%EB%B3%91%EC%9B%90%EC%9E%85%EA%B5%AC.%ED%95%9C%EA%B5%AD%EC%9E%90...%2C720-2%2C%EC%95%84%EC%A3%BC%EB%8C%80'
+};
+
+let dataString = 'cmd=searchBusStationJson&stationId=202000005';
+
+let url = 'http://www.gbis.go.kr/gbis2014/schBusAPI.action';
+let method = 'POST';
+let options = {
+    headers: headers,
+    body: dataString
+};
+
+function getStation() {
+    //options.body = 'cmd=searchBusStationJson&stationId='+ID;
+    response = request(method, url, options);
+    data = JSON.parse(response.body);
+    busList = data.result.busStationInfo;
+
+    return busList;
+}
+
+module.exports = getStation;
---
+++ query_analysis/query_analysis.js
@@ -0,0 +1,86 @@
+/**
+ * HHBOT BUS FIND Qeury Analysis module
+ * @author : dogfooter <gkstnrkf@gmail.com>
+ */
+const getRoute = require('./getRoute');
+const getBusList = require('./getStation');
+
+const BUS_REGEX_PAT = /\d{1,4}-?\d{0,3}/;
+/**
+ *
+ * @callback requestCallback
+ * @param {Int} err
+ * @param {String|Object} ret - for err message or return Bus Object
+ */
+
+
+/**
+ *  Analysis query and find bus direction and bus station
+ *  @param {String} query
+ *  @param {requestCallback} cb
+ */
+function query_analysis(query, cb) {
+    let dest = query.slice(0,3);
+    let busNum = query.match(BUS_REGEX_PAT);
+    let ret = null;
+    let err = null;
+
+    if(!busNum) {
+        err = 'Input right Bus Number in Query';
+        cb(-1, err);
+        return;
+    }
+    else {
+        busNum = busNum[0];
+    }
+
+    let busList = getBusList();
+    let Bus = null;
+
+    for (let i = 0; i < busList.length; i++){
+        if (busList[i].routeName == busNum){ // 버스 번호가 쿼리 문장에 있으면 참을 반환
+            Bus = busList[i].routeId;
+            break;
+        }
+    }
+
+    if (!Bus){
+        err = 'Input right Bus Number in Query';
+        cb(-1, err);
+    }
+
+
+    let stationList = getRoute(Bus);
+    let direction = null;
+
+    for (let i = 0; i < 2; i++){
+        for (let j = 0; j < stationList[i].length; j++){
+            if ((u = stationList[i][j].stationNm.indexOf(dest)) != -1){
+                console.log(stationList[i][j].stationNm.substring(u,stationList[i][j].stationNm.length));
+                direction = stationList[i][0];
+                break;
+            }
+        }
+    }
+
+
+    ret = {
+        'bus': {
+            'decode': busNum,
+            'encode': Bus
+        },
+        'stationFrom': direction
+    }
+
+    cb(0, ret);
+}
+
+module.exports = query_analysis;
+
+// useage
+/*
+query_analysis('수원역가는 730번', (err, ret) => {
+    if (err) console.log(ret);
+    console.log (ret);
+});
+*/
