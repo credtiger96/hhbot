66eff50ec8fef08f5b6db9a65225c141e56c3898
dogfooter <gkstnrkf@ajou.ac.kr>
2016-09-26 15:40:28 +0900

    variable_all_in_local

--- query_analysis/getBusList.js
+++ query_analysis/getBusList.js
@@ -1,28 +1,30 @@
 const request = require('request');
 
-let headers = {
-    'Origin': 'http://www.gbis.go.kr',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.92 Safari/537.36',
-    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-    'Accept': 'application/json, text/javascript, */*; q=0.01',
-    'Referer': 'http://www.gbis.go.kr/gbis2014/schBus.action?mapTabCd=1',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Connection': 'keep-alive'
-    // 'Cookie': 'PHAROSVISITOR=00004fee01570f1da75f36e9c0a867c9; ACEFCID=UID-57D2B892EA4906C8526E5EF1; weatherChange=2; JSESSIONID=Qx1gZaZzZhfrzcH9YeXYfqHtL16naw5JgGgQUY5LVK3a7FFoQtcIshjSIKQsuTGx.Gbus-WAS_servlet_engine3; _ga=GA1.3.524032906.1473427599; _gat=1; myword=%EC%95%84%EC%A3%BC%EB%8C%80.%EC%95%84%EC%A3%BC%EB%8C%80%EB%B3%91%EC%9B%90%EC%9E%85%EA%B5%AC.%ED%95%9C%EA%B5%AD%EC%9E%90...%2C720-2%2C%EC%95%84%EC%A3%BC%EB%8C%80'
-};
 
-let dataString = 'cmd=searchBusStationJson&stationId=202000005';
-;
-let options = {
-    url: 'http://www.gbis.go.kr/gbis2014/schBusAPI.action',
-    method: 'POST',
-    headers: headers,
-    body: dataString
-};
 
 function getBusList(cb) {
+    let headers = {
+        'Origin': 'http://www.gbis.go.kr',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.92 Safari/537.36',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Referer': 'http://www.gbis.go.kr/gbis2014/schBus.action?mapTabCd=1',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Connection': 'keep-alive'
+        // 'Cookie': 'PHAROSVISITOR=00004fee01570f1da75f36e9c0a867c9; ACEFCID=UID-57D2B892EA4906C8526E5EF1; weatherChange=2; JSESSIONID=Qx1gZaZzZhfrzcH9YeXYfqHtL16naw5JgGgQUY5LVK3a7FFoQtcIshjSIKQsuTGx.Gbus-WAS_servlet_engine3; _ga=GA1.3.524032906.1473427599; _gat=1; myword=%EC%95%84%EC%A3%BC%EB%8C%80.%EC%95%84%EC%A3%BC%EB%8C%80%EB%B3%91%EC%9B%90%EC%9E%85%EA%B5%AC.%ED%95%9C%EA%B5%AD%EC%9E%90...%2C720-2%2C%EC%95%84%EC%A3%BC%EB%8C%80'
+    };
+
+    let dataString = 'cmd=searchBusStationJson&stationId=202000005';
+    ;
+    let options = {
+        url: 'http://www.gbis.go.kr/gbis2014/schBusAPI.action',
+        method: 'POST',
+        headers: headers,
+        body: dataString
+    };
+    
     request(options, (err, res, body) => {
         if (err) throw err;
         let json_data = []
--- query_analysis/getRoute.js
+++ query_analysis/getRoute.js
@@ -1,34 +1,32 @@
-var request = require('request');
+const request = require('request');
 
-var headers = {
-    'Origin': 'http://www.gbis.go.kr',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36',
-    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-    'Accept': 'application/json, text/javascript, */*; q=0.01',
-    'Referer': 'http://www.gbis.go.kr/gbis2014/schBus.action?mapTabCd=3',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Connection': 'keep-alive'
-    // 'Cookie': 'myword=720-2; PHAROSVISITOR=0000601c01572eed1a531534c0a867c9; JSESSIONID=aJLhLc1R41zT2WbaUxYGI8DaXZd3IhtbMs1bifrFoMHkPflOQmdWqV2J8mDIuRWP.Gbus-WAS_servlet_engine2; _gat=1; ACEFCID=UID-57DADD4E626D6B51E9A7E7AE; weatherChange=2; _ga=GA1.3.1209589822.1473961289'
-
-};
-
-var dataString = 'cmd=searchRouteJson&routeId=234000026';
-
-var options = {
-    url: 'http://www.gbis.go.kr/gbis2014/schBusAPI.action',
-    method: 'POST',
-    headers: headers,
-    body: dataString
-
-};
 
 const fixed_station = '아주대.아주대병원';
 
 //request(options, callback);
 
 function getBusList(Bus, cb) {
+    let headers = {
+        'Origin': 'http://www.gbis.go.kr',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Referer': 'http://www.gbis.go.kr/gbis2014/schBus.action?mapTabCd=3',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Connection': 'keep-alive'
+        // 'Cookie': 'myword=720-2; PHAROSVISITOR=0000601c01572eed1a531534c0a867c9; JSESSIONID=aJLhLc1R41zT2WbaUxYGI8DaXZd3IhtbMs1bifrFoMHkPflOQmdWqV2J8mDIuRWP.Gbus-WAS_servlet_engine2; _gat=1; ACEFCID=UID-57DADD4E626D6B51E9A7E7AE; weatherChange=2; _ga=GA1.3.1209589822.1473961289'
+
+    };
+
+    let options = {
+        url: 'http://www.gbis.go.kr/gbis2014/schBusAPI.action',
+        method: 'POST',
+        headers: headers,
+
+    };
+
     options.body = 'cmd=searchRouteJson&routeId=' + Bus;
     request(options, (err, res, body) => {
         if (err) throw err;
